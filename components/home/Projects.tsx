eafc81724cd9ed8ed020b52cc08999cb3b1fd339
Johnny1305 <jonathan.ramos.business@gmail.com>
2024-01-11 07:59:12 +0100

    Updates

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -12,7 +12,7 @@ function Projects() {
         <SectionTitle title="Here are a few of my favorite projects." />
       </div>
       <div className="grid grid-cols-1 gap-12 md:gap-5 md:grid-cols-3 items-start">
-        {projects.slice(0, 3).map((item) => {
+        {projects.slice(-3).map((item) => {
           return (
             <ProjectCard key={item.id} project={item} />
           );
