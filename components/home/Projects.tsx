308aa28787b5cc3bff2b9ad1ff89f9e57c87906b
Johnny1305 <jonathan.ramos.business@gmail.com>
2024-11-11 13:28:20 +0100

    Update Projects.tsx

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -20,7 +20,7 @@ function Projects() {
       </div>
       <div className="relative w-full mt-2">
         <Link href="/projects">
-          <div className="mt-8 max-w-sm md:max-w-2xl border border-fun-pink mx-auto text-center w-full whitespace-nowrap px-8 py-3 rounded-full text-fun-pink bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white transition-colors cursor-pointer">
+          <div className="mt-8 max-w-sm md:max-w-2xl border border-fun-pink mx-auto text-center w-full whitespace-nowrap px-8 py-3 rounded-full text-fun-pink bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white transition-colors">
             View All
           </div>
         </Link>
